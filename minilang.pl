9be51e452789b81d11b93023c05ebaa14cc8e441
claralonguet <151842131+claralonguet@users.noreply.github.com>
2024-03-28 02:10:18 +0100

    Update minilang.pl

--- minilang.pl
+++ minilang.pl
@@ -1,20 +1,59 @@
 % Prédicat pour évaluer les expressions arithmétiques
-% eval(Expression, Etat, Valeur)
 eval(Expr, _, Expr) :-
     number(Expr). % Si l'expression est un nombre, retourne directement sa valeur
 
 eval(Var, Etat, Valeur) :-
     atom(Var), % Si l'expression est une variable
     member((Var, Valeur), Etat). % Recherche sa valeur dans l'état
+
 eval(Expr, Etat, Valeur) :-
     Expr =.. [Op, Gauche, Droite], % Si l'expression est une expression composée
+    member(Op, [+, -, *, /, **, mod]), % Vérifie si Op est un opérateur arithmétique
     eval(Gauche, Etat, ValeurGauche), % Évalue le côté gauche
     eval(Droite, Etat, ValeurDroite), % Évalue le côté droit
-    ExprEval =.. [Op, ValeurGauche, ValeurDroite], % Construit une nouvelle expression
-    Valeur is ExprEval. % Évalue la nouvelle expression
+    (   Op = mod ->
+        Valeur is ValeurGauche mod ValeurDroite % Évaluation de l'opérateur modulo
+    ;   ExprEval =.. [Op, ValeurGauche, ValeurDroite], 
+        Valeur is ExprEval
+    ).
+
+eval(Expr, Etat, true) :-
+    Expr =.. [>=, Gauche, Droite], % Si l'expression est une expression de comparaison >=
+    eval(Gauche, Etat, ValeurGauche), % Évalue le côté gauche
+    eval(Droite, Etat, ValeurDroite), % Évalue le côté droit
+    ValeurGauche >= ValeurDroite.
+
+eval(Expr, Etat, true) :-
+    Expr =.. [=<, Gauche, Droite], % Si l'expression est une expression de comparaison =<
+    eval(Gauche, Etat, ValeurGauche), % Évalue le côté gauche
+    eval(Droite, Etat, ValeurDroite), % Évalue le côté droit
+    ValeurGauche =< ValeurDroite.
+
+eval(Expr, Etat, true) :-
+    Expr =.. [<, Gauche, Droite], % Si l'expression est une expression de comparaison <
+    eval(Gauche, Etat, ValeurGauche), % Évalue le côté gauche
+    eval(Droite, Etat, ValeurDroite), % Évalue le côté droit
+    ValeurGauche < ValeurDroite.
+
+eval(Expr, Etat, true) :-
+    Expr =.. [>, Gauche, Droite], % Si l'expression est une expression de comparaison >
+    eval(Gauche, Etat, ValeurGauche), % Évalue le côté gauche
+    eval(Droite, Etat, ValeurDroite), % Évalue le côté droit
+    ValeurGauche > ValeurDroite.
+
+eval(Expr, Etat, true) :-
+    Expr =.. [=, Gauche, Droite], % Si l'expression est une expression de comparaison =
+    eval(Gauche, Etat, ValeurGauche), % Évalue le côté gauche
+    eval(Droite, Etat, ValeurDroite), % Évalue le côté droit
+    ValeurGauche =:= ValeurDroite.
+
+eval(Expr, Etat, true) :-
+    Expr =.. [=\=, Gauche, Droite], % Si l'expression est une expression de comparaison =\=
+    eval(Gauche, Etat, ValeurGauche), % Évalue le côté gauche
+    eval(Droite, Etat, ValeurDroite), % Évalue le côté droit
+    ValeurGauche =\= ValeurDroite.
 
 % Prédicat pour mettre à jour l'état du programme après une assignation de variable
-% update_state(Variable, Valeur, EtatPrecedent, NouvelEtat)
 update_state(Var, Val, [], [(Var, Val)]).
 update_state(Var, Val, [(Var, _)|T], [(Var, Val)|T]).
 update_state(Var, Val, [(X, V)|T], [(X, V)|NouvelleT]) :-
@@ -22,7 +61,6 @@ update_state(Var, Val, [(X, V)|T], [(X, V)|NouvelleT]) :-
     update_state(Var, Val, T, NouvelleT).
 
 % Prédicat pour exécuter les instructions MiniLang et mettre à jour l'état du programme
-% run(EtatPrecedent, Programme, EtatFinal)
 run(Etat, [], Etat). % Cas de base : si le programme est vide, l'état final est l'état actuel
 
 run(EtatPrecedent, Var = Expr, EtatFinal) :-
@@ -30,42 +68,43 @@ run(EtatPrecedent, Var = Expr, EtatFinal) :-
     update_state(Var, Valeur, EtatPrecedent, NouvelEtat),
     EtatFinal = NouvelEtat.
 
-run(EtatPrecedent, (Instruction; Suite), EtatFinal) :-
+run(EtatPrecedent, print(StringOrKey), EtatPrecedent) :-
+    (   atomic(StringOrKey) ->
+        (   member((StringOrKey, Value), EtatPrecedent) ->
+            write(Value)
+        ;   write(StringOrKey)
+        )
+    ;   eval(StringOrKey, EtatPrecedent, Value),
+        write(Value)
+    ),
+    nl.
+
+run(EtatPrecedent,(Instruction; Suite), EtatFinal) :-
     run(EtatPrecedent, Instruction, NouvelEtat),
     run(NouvelEtat, Suite, EtatFinal).
 
-run(EtatPrecedent, if(Cond, Alors)|T, EtatFinal) :-
+run(EtatPrecedent,if (Cond then(Then)), EtatFinal) :-
     (   eval(Cond, EtatPrecedent, true)
-    ->  run(EtatPrecedent, Alors, NouvelEtat)
-    ;   NouvelEtat = EtatPrecedent
-    ),
-    run(NouvelEtat, T, EtatFinal).
+    ->  run(EtatPrecedent, Then, EtatFinal)
+    ;   EtatFinal = EtatPrecedent
+    ).
 
-run(EtatPrecedent, if(Cond, Alors, Sinon)|T, EtatFinal) :-
+run(EtatPrecedent, if Cond Then() Else(), EtatFinal) :-
     (   eval(Cond, EtatPrecedent, true)
-    ->  run(EtatPrecedent, Alors, NouvelEtat)
-    ;   run(EtatPrecedent, Sinon, NouvelEtat)
-    ),
-    run(NouvelEtat, T, EtatFinal).
+    ->  run(EtatPrecedent, Then, EtatFinal)
+    ;   run(EtatPrecedent, Else, EtatFinal)
+    ).
 
-run(EtatPrecedent, while(Cond, Faire)|T, EtatFinal) :-
+run(EtatPrecedent, while(Cond, do(Do)), EtatFinal) :-
     (   eval(Cond, EtatPrecedent, true)
-    ->  run(EtatPrecedent, Faire, NouvelEtat1), % Exécute le bloc
-        run(NouvelEtat1, [while(Cond, Faire)], NouvelEtat) % Exécute la boucle while récursivement
-    ;   NouvelEtat = EtatPrecedent
-    ),
-    run(NouvelEtat, T, EtatFinal).
+    ->  run(EtatPrecedent, Do, NouvelEtat), % Exécute le bloc d'instructions
+        run(NouvelEtat, while(Cond, do(Do)), EtatFinal) % Réexécute la boucle avec le nouvel état
+    ;   EtatFinal = EtatPrecedent % Si la condition n'est plus vraie, retourne l'état précédent
+    ).
 
-
-    run_from_file( EtatPrecedent , FileName , EtatFinal ) :-
-        open( FileName , read , Stream ),
-        read_term(Stream , Prog , []) ,
-        close( Stream ),
-        run( EtatPrecedent , Prog , EtatFinal ).
-
-run(InitialState, print(Expr), FinalState) :-
-    (
-        eval(Expr,InitialState,Valeur)
-    ->  print(Valeur)
-    ),
-    run(InitialState, [], FinalState).
+% Prédicat pour exécuter les programmes à partir de fichiers
+run_from_file(InitialState, FileName, FinalState) :-
+    open(FileName, read, Stream),
+    read_term(Stream, Prog, []),
+    close(Stream),
+    run(InitialState, Prog, FinalState).
